7a02560087e487f1b68a6d66c54e2e415de24407
sinny777 <sinny777@gmail.com>
2018-07-28 18:40:17 +0530

    Code refactoring

--- machinelearning/tf-nlc-model/input_data_softmax.py
+++ machinelearning/tf-nlc-model/input_data_softmax.py
@@ -11,7 +11,7 @@ from os import path
 
 import nltk
 nltk.download('stopwords')
-# nltk.download('punkt')
+nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
