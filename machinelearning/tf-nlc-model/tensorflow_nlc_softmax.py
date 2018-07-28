7eeb78f97187cce0ca83dc60d988b575d342e48f
sinny777 <sinny777@gmail.com>
2018-07-28 15:27:46 +0530

    Small changes to import NLTK...

--- machinelearning/tf-nlc-model/tensorflow_nlc_softmax.py
+++ machinelearning/tf-nlc-model/tensorflow_nlc_softmax.py
@@ -30,7 +30,7 @@ import tarfile
 
 import nltk
 nltk.download('stopwords')
-nltk.download('punkt')
+# nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
