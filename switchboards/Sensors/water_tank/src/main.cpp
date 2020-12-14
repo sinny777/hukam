9b0462b469b84e028cab452dab95a9fc35e68ac8
Gurvinder Singh <gurvsin3@in.ibm.com>
2020-12-14 11:37:30 +0530

    small changes

--- switchboards/Sensors/water_tank/src/main.cpp
+++ switchboards/Sensors/water_tank/src/main.cpp
@@ -1,20 +1,27 @@
+
 /*
  circuitdigest.com
- Sample STM32 Blink Program for Blue Pill board 
+ Sample STM32 Blink Program for Blue Pill board
  */
 
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include <SPI.h>              // include libraries
+// #include <LoRa.h>
 #include <LoRa_STM32.h>
 
-#define LED_BUILTIN PC13
-#define echoPin PA8 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin PA9 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define LED_BUILTIN_1 PB15
+#define echoPin PB10 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin PB11 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define light_sensor PB0
 
-const int csPin = PA4;          // LoRa radio chip select
-const int resetPin = PC14;       // LoRa radio reset
-const int irqPin = PA1;         // change for your board; must be a hardware interrupt pin
+#define csPin  PA4          // LoRa radio chip select
+#define resetPin  PC13       // LoRa radio reset
+#define irqPin  PA0  // change for your board; must be a hardware interrupt pin
+#define bat_pin PA1   // to get the current Battery Volts
+#define charge_pin PB14
+
+boolean charge = false;
 
 String outgoing;              // outgoing message
 
@@ -36,6 +43,7 @@ void sendMessage(String outgoing) {
   // msgCount++;                           // increment message ID
 }
 
+
 // returns the distance (cm)
 void measureDistance() {
   digitalWrite(trigPin, LOW);
@@ -46,8 +54,8 @@ void measureDistance() {
   digitalWrite(trigPin, LOW);
 
   uint32_t duration = pulseIn(echoPin, HIGH, 25000); // We wait for the echo to come back, with a timeout of 20ms, which corresponds approximately to 3m
-  // long duration = pulseIn(echoPin, HIGH); 
-  
+  // long duration = pulseIn(echoPin, HIGH);
+
   // pulseIn will only return 0 if it timed out. (or if echoPin was already to 1, but it should not happen)
   // If we timed out
   if(duration == 0) {
@@ -58,30 +66,43 @@ void measureDistance() {
   }
 
   // float distance = (duration/2) / 29.1; // We calculate the distance (sound speed in air is aprox. 291m/s), /2 because of the pulse going and coming
-  float distance = duration * 0.034 / 2; 
-  // float distance = duration / 58.138f; 
+  float distance = duration * 0.034 / 2;
+  // float distance = duration / 58.138f;
 
   // String payload = "{\"type\":\"" BOARD_TYPE "\", \"uniqueId\":\"" +BOARD_ID+"\", \"deviceIndex\":1, \"deviceValue\": " +String(sw1Val)+"}";
   String payload;
   StaticJsonDocument<200> data;
-  // JsonObject& jsonOut = dataJsonBuffer.createObject();
-  data["distance"] = distance;  
+//  JsonObject& jsonOut = dataJsonBuffer.createObject();
+  
+  data["distanc"] = distance;
+  data["light"]=analogRead(light_sensor);
   String output;
   serializeJson(data, output);
-  sendMessage(output); 
+  sendMessage(output);
+}
+
+void checkBattery() {
+   StaticJsonDocument<200> bat_status;
+   bat_status["battery_status"]=analogRead(bat_pin);
+   String output=" ";
+   serializeJson(bat_status,output);
+   sendMessage(output);  
 }
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin PC13 as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize digital pin LED_BUILTIN  as an output.
+  pinMode(LED_BUILTIN_1, OUTPUT);
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  pinMode(light_sensor,INPUT);
+  pinMode(charge_pin,OUTPUT);
+  pinMode(bat_pin,INPUT);
 
-  LoRa.setPins(csPin, resetPin, irqPin);// set CS, reset, IRQ pin
-
+ LoRa.setPins(csPin, resetPin, irqPin);// set CS, reset, IRQ pin
+    
   if (!LoRa.begin(433E6)) {             // initialize ratio at 915 MHz
-    // Serial.println("LoRa init failed. Check your connections.");
+     Serial.println("LoRa init failed. Check your connections.");
     while (true);                       // if failed, do nothing
   }
 
@@ -89,10 +110,10 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);   
-  // sendMessage("Hello World...");           // wait for a second
+  digitalWrite(LED_BUILTIN_1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);
+  checkBattery();
   measureDistance();
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN_1, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);              // wait for a second
 }
